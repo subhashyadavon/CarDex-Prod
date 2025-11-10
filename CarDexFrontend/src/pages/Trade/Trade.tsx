78d82192d71750f3f2ec6e4aee8aa46c693416ac
Vansh Shah <vanshshah30@gmail.com>
2025-11-10 13:46:38 -0600

    Separate Trade component from App

---
+++ CarDexFrontend/src/pages/Trade/Trade.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TradeSection: React.FC = () => {
+  return (
+    <p className="body-1" style={{ color: "var(--content-secondary)" }}>
+      Trade center coming soon.
+    </p>
+  );
+};
+
+export default TradeSection;
